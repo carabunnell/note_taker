6a2b82ed21d59612aeb4b4fa7dc175c8a10da825
Cara Bunnell <carabunnell@gmail.com>
2020-03-31 01:16:33 -0700

    requiring new note class

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,14 +1,18 @@
 // api routes. 
-var noteData = require("../public/assets/notes.js")(app);
+var noteData = require("../public/assets/notes.js");
+// var noteIndex = require("../public/index.html")
 
-module.exports = function(app) {
-    app.get("/api/notes", function(req, res) {
-        res.json(noteData);
-    });
-    app.post("/api/notes", function(req, res) {
-        res.json(true);
-    });
-    app.delete("api/notes/" + id, function(req, res) {
-        res.json(noteData + id);
-    });
-}
+module.exports = function (app) {
+        app.get("/api/notes", function (req, res) {
+            res.json(noteData);
+        });
+        app.post("/api/notes", function (req, res) {
+            res.json(true);
+        });
+        // app.delete("api/notes/:id", function(req, res) {
+        //     res.json(noteData + id);
+        // });
+        //     app.get("/*", function(req, res) {
+        //         res.json(noteIndex);
+        //     });
+        }
