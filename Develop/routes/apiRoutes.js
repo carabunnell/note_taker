4866304245e33358e5193accbea66f2cb8f1f628
Cara Bunnell <carabunnell@gmail.com>
2020-05-31 10:08:55 -0700

    deleting the readfile before pushing

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -25,29 +25,47 @@ module.exports = function (app) {
             res.status(200).json(parsed);
         });
     });
+
+
     app.post("/api/notes", function (req, res) {
         //adding a new note to json and reading it. 
-        console.log("filed notes:", filedNotes.length)
+        // console.log("filed notes:", filedNotes.length);
+        //reading file
+        
+        // fs.readFileSync(filedNotes, "utf8", (err, data) => {
+        //     if (err) throw err;
+        //     var parsed = JSON.parse(data);
+        //     console.log(parsed);
+        // });
+
+
+        //consologing the parsed data to find the id of last input.
+        // console.log(obj);
         var newNote = {
-            id: 3,
+            id: Date.now(),
             title: req.body.title,
             text: req.body.text
         }
         console.log(newNote);
 
+        //reading file
         var data = fs.readFileSync(filedNotes, "utf8");
-
+        //parsing out file after reading file
         var obj = JSON.parse(data);
+        //pushing new note to the parsed file
         obj.push(newNote);
+        //moving data to a string instead of an object...i think
         var objString = JSON.stringify(obj, null, 2);
 
+        //now rewriting the new note to the file in string format.
         fs.writeFile(filedNotes, objString, "utf8", err => {
             if (err) throw err;
             console.log("Successfully added note!");
         });
-         //reading notes from json
+         //reading and rendering notes from json to site
          fs.readFile(filedNotes, "utf8", (err, data) => {
             if (err) throw err;
+            console.log(data);
             var parsed = JSON.parse(data);
             res.status(200).json(parsed);
         });
